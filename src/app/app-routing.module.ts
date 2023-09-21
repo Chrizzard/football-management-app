c868aa353c9836a3e64e8ab9ed41c57931eedc8f
Sam Boosten <boostensam@gmail.com>
2023-09-21 15:12:11 +0200

    player info page basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerInfoPageComponent } from './player-info-page/player-info-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'players/:id', component: PlayerInfoPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,26 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MatchesSideBarComponent } from './matches-side-bar/matches-side-bar.component';
 import { LogoComponent } from './logo/logo.component';
 import { MatchesSideBarElementComponent } from './matches-side-bar/matches-side-bar-element/matches-side-bar-element.component';
+import { PlayersComponent } from './players/players.component';
+import { PlayerCardComponent } from './players/player-card/player-card.component';
+import { PlayerInfoPageComponent } from './player-info-page/player-info-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     MatchesSideBarComponent,
-    LogoComponent
+    LogoComponent,
+    PlayersComponent,
+    PlayerCardComponent,
+    PlayerInfoPageComponent
     ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
