7613aa8ec5a92c87661066e775d97a33d599ab1c
Hon Man Wong <honman-wong@hotmail.com>
2023-09-27 13:57:31 +0200

    added styling

--- src/app/matches/matches.component.ts
+++ src/app/matches/matches.component.ts
@@ -5,17 +5,24 @@ import { MatchesService } from './matches.service';
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
-  styleUrls: ['./matches.component.scss']
+  styleUrls: ['./matches.component.scss'],
 })
 export class MatchesComponent {
   matches: Match[] = [];
+  searchText: string = '';
 
-  constructor(private matchesService: MatchesService){}
+  constructor(private matchesService: MatchesService) {
+    this.matches = [];
+  }
 
-  ngOnInit(): void{
-    this.matchesService.getAllMatches().subscribe(matches => {
+  ngOnInit(): void {
+    this.matchesService.getAllMatches().subscribe((matches) => {
       this.matches = matches;
-      console.log(this.matches)
-    })
+      console.log(this.matches);
+    });
+  }
+
+  performSearch() {
+    
   }
 }
